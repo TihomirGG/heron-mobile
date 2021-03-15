17d9ad01cbe8f6ed2ea12554cf648609314d5608
Tihomir Gospodinov <tihomir.gospodinov@abv.bg>
2021-03-15 21:43:01 +0200

    works with firebase. Also register works.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.scss';
-import Header from './Components/Header/header';
 import Home from './Pages/Home/Home';
-import Login from './Pages/Login/Login'
+import Login from './Pages/Login/Login';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Register from './Pages/Register';
+import ROUTES from './Constants/Routes';
+import Profile from './Pages/Profile';
+import About from './Pages/About';
+import { useContext } from 'react';
+import { FirebaseContext } from './Firebase';
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/login">
-            <Login/>
-          </Route>
-          <Route exact path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
-  );
+function App(props) {
+    const { logOutUser } = useContext(FirebaseContext);
+
+    return (
+        <div className="app" value={props.value}>
+            {console.log(props.value)}
+            <Router>
+                <Switch>
+                    <Route exact path={ROUTES.SIGN_UP}>
+                        <Register />
+                    </Route>
+                    <Route exact path={ROUTES.SING_IN}>
+                        <Login />
+                    </Route>
+                    <Route exact path={ROUTES.HOME}>
+                        <Home />
+                    </Route>
+                    <Route exact path={`${ROUTES.PROFILE}:id`}>
+                        <Profile />
+                    </Route>
+                    <Route exact path={ROUTES.ABOUT}>
+                        <About />
+                    </Route>
+                    <Route>{logOutUser()}</Route>
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
